6322c1975defac6cbcd0b5fa6f55c4b31a98862b
jefrayn <salinasj.jr@gmail.com>
2025-03-09 11:51:25 -0400

    final committ

--- app/components/Table.jsx
+++ app/components/Table.jsx
@@ -1,6 +1,7 @@
 
 function Table(props){
 
+
     return(
         // Some code will go in here
         <table>
@@ -13,25 +14,17 @@ function Table(props){
             </thead>
 
             <tbody>
-                <tr>
-                    <td>Github</td>
-                    <td>https://www.github.com</td>
-                </tr>
+               
+               { 
+                    props.data.map((favLink, index)=>{
+                    
+                       return(<tr key="">
+                            <td>{favLink.name}</td>
+                            <td>{favLink.URL}</td>
+                        </tr>)
 
-                <tr>
-                    <td>Google</td>
-                    <td>https://www.google.com</td>
-                </tr>
-
-                <tr>
-                    <td>Amazon</td>
-                    <td>https://www.amazon.com</td>
-                </tr>
-
-                <tr>
-                    <td>{props.data.name}</td>
-                    <td>{props.data.URL}</td>
-                </tr>
+                    })
+                }
 
             </tbody>
 
