ac321aa1bf39efbf7ed679071867f99dc3d5098b
Marwa <marwaemreish@gmail.com>
2025-03-10 22:23:29 -0400

    final commit

--- app/components/Form.jsx
+++ app/components/Form.jsx
@@ -1,61 +1,59 @@
-import {useState} from 'react'
+
+import {useState} from 'react'  //a hook
+
 
 function Form(props){
+    //state - a way for us to set and reference data that changes in our component
+    const[name, setName] = useState("")
+    const[URL, setURL] = useState("")
 
-    // state - a way for us to set and reference data that changes in our component
-    const [name, setName] = useState("")
-    const [URL, setURL] = useState("")
-    
     function handleClick(){
-       // reference the data inside of the input
+        //alert("Clicked!")
+        //reference thae data inside of the input
+        //console.log(name, URL)
 
-        //    console.log(name, URL)
-
-        // alert the parent component to pass some data as props to the table
-        // take the last value of the input and send it to the table
-
-        // let favLink = {
-        //     name: name,
-        //     URL: URL
-        // }
-
+        //take the last value of the input and send it to the table
         props.submitFavLink({name, URL})
-
+        
     }
 
-
     function handleNameInputChange(event){
-        // we can use the event object to look at our input
+        //we can use the event object to look at our input
         console.log(event.target.value)
-        // save this data for later
-        setName(event.target.value) // update the state with our data 
+        //alert("input was changed")
+
+
+        //save this data for later
+        setName(event.target.value) //update the state with our data
     }
 
     function handleURLInputChange(event){
-        // we can use the event object to look at our input
+        //we can use the event object to look at our input
         console.log(event.target.value)
-        // save this data for later
-        setURL(event.target.value) // update the state with our data
+        //alert("input was changed")
+
+
+        //save this data for later
+        setURL(event.target.value) //update the state with our data
     }
 
-    
+
     return(
-        <div>
-            {/* Form for the user to input data */}
+        <div> 
+            { /* form for the user to input data*/}
             <form>
                 <label> Name </label>
-                <input type="text" onChange={handleNameInputChange}/>
+                <input type="text" onChange={handleNameInputChange}/> {/*onChange, onSubmit, onClick are event listeners*/}
 
                 <label> URL </label>
                 <input type="text" onChange={handleURLInputChange}/>
+
+                    
             </form>
-
-            <button onClick={handleClick}> Submit </button>
-
-        </div>
+                
+                <button onClick={handleClick}> Submit </button> {/*run this function*/}
+            </div>
     )
 }
 
 export default Form
-
-
