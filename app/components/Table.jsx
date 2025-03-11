373951fb8da789c32dfe32597e7cd48a9e19250b
prasangatiwari <prasangahere@gmail.com>
2025-03-10 23:53:30 -0400

    Final push

--- app/components/Table.jsx
+++ app/components/Table.jsx
@@ -1,43 +1,26 @@
-
-function Table(props){
-
+function Table(props) {
     return(
-        // Some code will go in here
         <table>
-            
             <thead>
-                <tr>
-                    <th>Name</th>
-                    <th>URL</th>
-                </tr>
             </thead>
-
             <tbody>
-                <tr>
-                    <td>Github</td>
-                    <td>https://www.github.com</td>
-                </tr>
 
-                <tr>
-                    <td>Google</td>
-                    <td>https://www.google.com</td>
-                </tr>
+                {
+                    props.data.map((favLink, index)=> {
+                        return (
+                            <tr key={index}>
+                                <td>{favLink.name}</td>
+                                <td>{favLink.URL}</td>
+                            </tr>)
 
-                <tr>
-                    <td>Amazon</td>
-                    <td>https://www.amazon.com</td>
-                </tr>
-
-                <tr>
-                    <td>{props.data.name}</td>
-                    <td>{props.data.URL}</td>
-                </tr>
-
+                    })
+                }
             </tbody>
 
+
         </table>
+
     )
 
 }
-
-export default Table
+export default Table;
--- app/page.jsx
+++ app/page.jsx
@@ -1,44 +1,31 @@
 // Components are functions that return some JSX
 
 // JSX is a javascript syntax in React to create our HTML elements
-
 "use client";
+import { useState } from 'react';
 
-import { useState } from "react" // we use this to add state to our components
-// state lets us keep track of changing data and show it in the component
+import Form from "./components/form";
+import Table from "./components/Table";
 
-import Form from "./components/Form"
-import Table from "./components/Table"
+function Homepage() {
 
-function HomePage(){   
-
-    const [newFavLink, setNewFavLink] = useState({})
-    
-    function handleNewFavLink(favLink){
-        // favlink is an object containing a {name, URL}
-
-        console.log(favLink, "in HomePage")
-
-        setNewFavLink(favLink)
+    const [favLinks, setFavLinks] = useState([]);
+    function handleNewFavLink(favLink) {
+        console.log(favLink, "in HomePage");
+        let newFavLinks = [...favLinks, favLink];
+        setFavLinks(newFavLinks);
     }
-    
 
     return (
         <div>
-            <h1> FavLinks </h1>
-       
-       {/* The Form is responsible for gathering the data
-        and alerting the HomePage when it needs to pass it to the table  */}
-           
-            <Form submitFavLink={handleNewFavLink} /> 
+            <h1>FavLinks</h1>
 
-            
-            
-            <Table data={newFavLink}/>
-            
+            <Form submitFavLink={handleNewFavLink} />
 
+            <Table data={favLinks} />
 
         </div>
-    )
+    );
 }
-export default HomePage
+
+export default Homepage;
