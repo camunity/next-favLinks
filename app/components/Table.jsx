5cf66722ace8a8ccfecedc810882b47f3d7a683c
Versa <versa@Versas-MacBook-Pro.local>
2025-03-10 22:42:05 -0400

    final commit

--- app/components/Table.jsx
+++ app/components/Table.jsx
@@ -5,33 +5,23 @@ function Table(props){
         // Some code will go in here
         <table>
             
-            <thead>
-                <tr>
-                    <th>Name</th>
-                    <th>URL</th>
-                </tr>
+            <thead>-
             </thead>
+                
 
             <tbody>
-                <tr>
-                    <td>Github</td>
-                    <td>https://www.github.com</td>
-                </tr>
+                
 
-                <tr>
-                    <td>Google</td>
-                    <td>https://www.google.com</td>
-                </tr>
-
-                <tr>
-                    <td>Amazon</td>
-                    <td>https://www.amazon.com</td>
-                </tr>
-
-                <tr>
-                    <td>{props.data.name}</td>
-                    <td>{props.data.URL}</td>
-                </tr>
+                {
+                    props.data.map((favLink, index )=>{
+                    
+                        return(<tr key={index}>
+                        <td>{favLink.name}</td>
+                        <td>{favLink.URL}</td>
+                    </tr>)
+                    })
+            
+                }
 
             </tbody>
 
