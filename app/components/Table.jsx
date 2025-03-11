5c1b88678f714c78ee9bfc7264cda551ee4bae62
Lehman College <Lehman@ridas-mbp.mynetworksettings.com>
2025-03-10 23:39:21 -0400

    final commit

--- app/components/Table.jsx
+++ app/components/Table.jsx
@@ -1,37 +1,33 @@
 
 function Table(props){
 
+    
+
     return(
         // Some code will go in here
         <table>
             
             <thead>
-                <tr>
+            <tr>
                     <th>Name</th>
                     <th>URL</th>
+
                 </tr>
             </thead>
 
             <tbody>
-                <tr>
-                    <td>Github</td>
-                    <td>https://www.github.com</td>
-                </tr>
 
-                <tr>
-                    <td>Google</td>
-                    <td>https://www.google.com</td>
-                </tr>
+                {
+                    props.data.map((favLink, index )=>{
 
-                <tr>
-                    <td>Amazon</td>
-                    <td>https://www.amazon.com</td>
-                </tr>
+                return(<tr key={index}>
+                    <td>{favLink.name}</td>
+                    <td>{favLink.URL}</td>
+                </tr>)
 
-                <tr>
-                    <td>{props.data.name}</td>
-                    <td>{props.data.URL}</td>
-                </tr>
+                    })
+                
+                }
 
             </tbody>
 
