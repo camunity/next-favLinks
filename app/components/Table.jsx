ec588e471e3cfa6c7f85cb1613776f777cdb30aa
Ron <ronaddriel@Rons-MacBook-Pro.local>
2025-03-10 20:54:10 -0400

    final commit

--- app/components/Table.jsx
+++ app/components/Table.jsx
@@ -1,43 +1,40 @@
-
 function Table(props){
 
+
     return(
-        // Some code will go in here
         <table>
-            
             <thead>
                 <tr>
                     <th>Name</th>
                     <th>URL</th>
                 </tr>
             </thead>
-
             <tbody>
-                <tr>
-                    <td>Github</td>
-                    <td>https://www.github.com</td>
+            {
+                props.data.map((favlink, index)=>{
+                   
+              
+                return(<tr key={index}>
+                    <td>{favlink.name}</td>
+                    <td>{favlink.url}</td>
                 </tr>
-
-                <tr>
-                    <td>Google</td>
-                    <td>https://www.google.com</td>
-                </tr>
-
-                <tr>
-                    <td>Amazon</td>
-                    <td>https://www.amazon.com</td>
-                </tr>
-
-                <tr>
-                    <td>{props.data.name}</td>
-                    <td>{props.data.URL}</td>
-                </tr>
-
+                )
+ 
+ 
+            })
+        }
             </tbody>
-
+ 
+ 
         </table>
     )
-
-}
-
-export default Table
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ }
+ export default Table
